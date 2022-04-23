5353683ca4bedf5c90e4d0f4db9aff16dba3df35
Adnan Vena <sjor.venandi@outlook.com>
2022-04-23 16:49:30 +0200

    V1

---
+++ eProdaja.Services/ProizvodiService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eProdaja.Services
+{
+    public class ProizvodiService : IProizvodiService
+    {
+        public List<Proizvodi> ProizvodiList =new List<Proizvodi> { new Proizvodi() { Id = 1, Naziv = "Laptop" }, new Proizvodi() { Id = 2, Naziv = "Miš" } };
+        public IEnumerable<Proizvodi> Get()
+        {
+            ProizvodiList.Add(new Proizvodi() { Naziv ="Test01",Id = 0});
+            return ProizvodiList;
+        }
+        public Proizvodi GetById(int id)
+        {
+            return ProizvodiList.FirstOrDefault(x=>x.Id==id);
+        }
+    }
+}
